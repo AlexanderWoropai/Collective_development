a307e07eb6e916bdd48e6115e84d2ce63bfbcb81
Alexander <aworopei@mail.ru>
2022-03-01 14:04:30 +0300

    versionOfAlexander

---
+++ CRiUPP_lab_01/CRiUPP_lab_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRiUPP_lab_01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Collections.Generic.Stack<int> s = new System.Collections.Generic.Stack<int>(new int[] { 1,2,3});
+
+
+      List<int> list = new List<int>();
+      LinkedList<int> llist = new LinkedList<int>();
+
+      //int a = list[1];
+      //int b = llist.Last.Value;
+
+      Console.WriteLine(s.Contains(2));
+      Console.WriteLine(s.Contains(7));
+    }
+  }
+}
